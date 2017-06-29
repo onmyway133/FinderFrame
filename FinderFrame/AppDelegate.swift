6fd179b25d81358eac0fc066c45d5d8d8e8aeb5d
Khoa Pham <onmyway133@gmail.com>
2017-06-29 15:33:16 +0200

    Support macOS 10.11

--- FinderFrame/AppDelegate.swift
+++ FinderFrame/AppDelegate.swift
@@ -14,11 +14,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   @IBOutlet weak var saveMenuItem: NSMenuItem!
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
-  }
 
-  func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
+    NSUserNotificationCenter.default.delegate = self
   }
 }
 
