f1affdc0165a0b64ecba3077b26b40491c7d556f
Khoa Pham <onmyway133@gmail.com>
2017-06-29 11:44:13 +0200

    Initial commit

---
+++ FinderFrame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FinderFrame
+//
+//  Created by Khoa Pham on 29.06.2017.
+//  Copyright © 2017 Fantageek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# FinderFrame
+
+## Author
+
+Khoa Pham, onmyway133@gmail.com
+
+## License
+
+**FinderFrame** is available under the MIT license. See the [LICENSE](https://github.com/onmyway133/FinderFrame/blob/master/LICENSE.md) file for more info.
