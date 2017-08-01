e428ce37933454aec60c7a038cf28a9d4cbe80c1
Khoa Pham <onmyway133@gmail.com>
2017-08-01 16:32:46 +0200

    Add toBitmap function

--- FinderFrame/Library/Extensions.swift
+++ FinderFrame/Library/Extensions.swift
@@ -1,9 +1,17 @@
 import Cocoa
 
 extension NSImage {
-  func toData() -> Data? {
+  func toBitmap() -> NSBitmapImageRep? {
     guard let tiffData = tiffRepresentation,
-      let bitmap = NSBitmapImageRep(data: tiffData),
+      let bitmap = NSBitmapImageRep(data: tiffData) else {
+        return nil
+    }
+
+    return bitmap
+  }
+
+  func toData() -> Data? {
+    guard let bitmap = toBitmap(),
       let imageData = bitmap.representation(using: .PNG, properties: [:]) else {
         return nil
     }
